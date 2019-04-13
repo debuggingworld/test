27983b5f11e06ba90b84d1b84e052dc852941058
Zhangtao153 <13289265979@163.com>
2019-04-13 18:47:21 +0800

    修改并保存DOM

--- xml/src/com/zth/Demo04.java
+++ xml/src/com/zth/Demo04.java
@@ -1,32 +1,38 @@
 package com.zth;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Text;
+import org.w3c.dom.*;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.FileOutputStream;
+
 
 /**
  * DOM 修改 XML 文件后写入磁盘
  */
 public class Demo04 {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
 
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 
         DocumentBuilder documentBuilder = factory.newDocumentBuilder();
         Document document = documentBuilder.parse("res/stus.xml");
 
-        Element stus = (Element)document.getElementsByTagName("stus").item(0);
+
+        Element stus = (Element) document.getElementsByTagName("stus").item(0);
 
         Element e = document.createElement("stu");
         stus.appendChild(e);
-        Element newstu = (Element)document.getElementsByTagName("stus").item(0).getLastChild();
+        Element newstu = (Element) document.getElementsByTagName("stus").item(0).getLastChild();
+        Element name = document.createElement("name");
+        Element age = document.createElement("age");
+        Element sex = document.createElement("sex");
 
-        Element name = document.createElement("stu");
-        Element age = document.createElement("stu");
-        Element sex = document.createElement("stu");
+        name.setAttribute("id", "2");
 
         Text nameText = document.createTextNode("咯咯");
         Text ageText = document.createTextNode("18");
@@ -36,5 +42,27 @@ public class Demo04 {
         newstu.appendChild(age);
         newstu.appendChild(sex);
 
+        name.appendChild(nameText);
+        age.appendChild(ageText);
+        sex.appendChild(sexText);
+
+        saveXML(document);
+
     }
+
+    public static void saveXML(Document document) throws Exception {
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+
+        Transformer transformer = transformerFactory.newTransformer();
+        DOMSource domSource = new DOMSource(document);
+
+        FileOutputStream out = new FileOutputStream("res/stus1.xml");
+        StreamResult streamResult = new StreamResult(out);
+
+        transformer.transform(domSource, streamResult);
+
+        out.close();
+
+    }
+
 }
