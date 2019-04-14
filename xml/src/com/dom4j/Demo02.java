de42d9295c8e9441e968a43e3cf332a8072d25cd
Zhangtao153 <13289265979@163.com>
2019-04-14 12:45:18 +0800

    通过DOM4J操作XML

--- xml/src/com/dom4j/Demo02.java
+++ xml/src/com/dom4j/Demo02.java
@@ -1,24 +1,23 @@
 package com.dom4j;
-
-import org.dom4j.Document;
-import org.dom4j.Element;
-import org.dom4j.io.DOMReader;
-import org.dom4j.io.SAXReader;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import java.io.File;
-import java.util.List;
+        import org.dom4j.Document;
+        import org.dom4j.Element;
+        import org.dom4j.io.DOMReader;
+        import org.dom4j.io.SAXReader;
+        import javax.xml.parsers.DocumentBuilder;
+        import javax.xml.parsers.DocumentBuilderFactory;
+        import java.io.File;
+        import java.util.Iterator;
+        import java.util.List;
 
 public class Demo02 {
     public static void main(String[] args) throws Exception{
 
         //SAXReader 类来构造dom4j 文档对象
         SAXReader saxReader = new SAXReader();
-        org.dom4j.Document document = saxReader.read(new File("res/stus.xml"));
+        org.dom4j.Document document = saxReader.read(new File("res/stus2.xml"));
 
         //DOMReader类构造dom4j文档对象
-       /* DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+      /*  DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         DocumentBuilder documentBuilder = factory.newDocumentBuilder();
         org.w3c.dom.Document document1= documentBuilder.parse("res/stus.xml");
 
@@ -30,6 +29,18 @@ public class Demo02 {
         //root.element("stu");
         List<Element> elements = root.elements();
 
+        Iterator iterator = root.elementIterator();
+
+        while (iterator.hasNext()){
+            Element stu = (Element)iterator.next();
+
+            System.out.println("学号："+stu.attributeValue("id"));
+            System.out.println("姓名："+stu.elementText("name"));
+            System.out.println("年龄："+stu.elementText("age"));
+            System.out.println("性别："+stu.elementText("sex"));
+
+            System.out.println("+++++++++++++++++++");
+        }
 
     }
 }
